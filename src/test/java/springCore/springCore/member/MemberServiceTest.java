5ca2fdf23ca828f515f16d0f380b55150b2fff03
wnstn819 <wnstn819@naver.com>
2023-01-03 22:48:21 +0900

    no message

--- src/test/java/springCore/springCore/member/MemberServiceTest.java
+++ src/test/java/springCore/springCore/member/MemberServiceTest.java
@@ -7,7 +7,7 @@ public class MemberServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
     @Test
-    void join(){
+    void join(){ // 테스트 3단계
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
@@ -17,5 +17,7 @@ public class MemberServiceTest {
 
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
+
+
     }
 }
